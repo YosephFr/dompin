a3fecb59d7d6e5afb44efe9d6d494fd0230d6360
Yoseph Franco <yosephfr@icloud.com>
2026-05-05 11:34:23 -0300

    style: prettier sweep on vault-handle

--- packages/extension/src/common/vault-handle.ts
+++ packages/extension/src/common/vault-handle.ts
@@ -4,12 +4,12 @@ declare global {
   }
 
   interface FileSystemHandle {
-    queryPermission(descriptor?: FileSystemHandlePermissionDescriptor): Promise<
-      'granted' | 'denied' | 'prompt'
-    >;
-    requestPermission(descriptor?: FileSystemHandlePermissionDescriptor): Promise<
-      'granted' | 'denied' | 'prompt'
-    >;
+    queryPermission(
+      descriptor?: FileSystemHandlePermissionDescriptor,
+    ): Promise<'granted' | 'denied' | 'prompt'>;
+    requestPermission(
+      descriptor?: FileSystemHandlePermissionDescriptor,
+    ): Promise<'granted' | 'denied' | 'prompt'>;
   }
 
   interface ShowDirectoryPickerOptions {
